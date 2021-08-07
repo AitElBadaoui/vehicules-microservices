92eb589767425ea20d34b6a09fe633ea110c48e1
Mouad <mouad.aitelbad@gmail.com>
2021-08-07 19:48:45 +0200

    feat: add eureka server

---
+++ P02-VehiclesAPI/server/src/test/java/com/eureka/server/ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
